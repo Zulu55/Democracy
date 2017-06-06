95615ca19da45e581331044dae360241c7f43159
Zulu <jzuluaga55@gmail.com>
2017-06-05 20:30:46 -0500

    Finish

---
+++ Democracy/Controllers/StatesController.cs
@@ -0,0 +1,61 @@
+﻿using Democracy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Democracy.Controllers
+{
+    public class StatesController : Controller
+    {
+        /// <summary>
+        /// The data base connection
+        /// </summary>
+        private DemocracyContext db = new DemocracyContext();
+
+        /// <summary>
+        /// List all states
+        /// </summary>
+        /// <returns>The view</returns>
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(db.States.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Creates a new state
+        /// </summary>
+        /// <param name="state">The state</param>
+        /// <returns>The view</returns>
+        [HttpPost]
+        public ActionResult Create(State state)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(state);
+            }
+
+            db.States.Add(state);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            
+            base.Dispose(disposing);
+        }
+	}
+}
---
+++ Democracy/Models/State.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Democracy.Models
+{
+    /// <summary>
+    /// State model
+    /// </summary>
+    public class State
+    {
+        /// <summary>
+        /// Gets or sets the state id
+        /// </summary>
+        [Key]
+        public int StateId { get; set; }
+
+        /// <summary>
+        /// Gets or setS the state descripton
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
